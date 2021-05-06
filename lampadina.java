386375ea0852f0caef349173855bcf7301683b08
Gian Marco <49276912+Metallium96@users.noreply.github.com>
2021-05-06 16:21:27 +0200

    Create lampadina.java

---
+++ lampadina.java
@@ -0,0 +1,61 @@
+public class FSMLampadina {
+	private FSMController ctr;
+	private FSMState st = Off.OFF
+	
+	// definizione degli eventi
+	public void accendi() {
+		st.accendi(this);
+	}
+	
+	public void spegni() {
+		st.spegni(this);
+	}
+	
+	// definizione delle azioni
+	public void click(){
+		controller.click();
+	}
+	
+	// per aggiornare “state”
+	setst(FSMstate s) {
+		st = s;
+	}
+}
+
+//Interfaccia comune
+public interface FSMState {
+	public void accendi(FSMLampadina: f);
+	public void spegni(FSMLampadina: f);
+}
+
+//Stato acceso
+public class on implements FSMState {
+	public void spegni (FSMLampadina f) {
+		f.setst(Off.OFF);
+		f.click();
+	}
+	
+	public void accendi (FSMLampadina f) {
+		f.click();
+	}
+}
+
+//Stato spento
+public class off implements FSMState {
+	public void accendi (FSMLampadina f) {
+		f.setst(On.ON);
+		f.click();
+	}
+	
+	public void spegni (FSMLampadina f) {
+		f.click();
+	}
+}
+
+
+//Controller
+public class FSMController {
+	public void click(){
+		System.out.println("*click*");
+	}
+}
