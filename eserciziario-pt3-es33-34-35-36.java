a5f6e5120e70fb8e030d0e8b00599f9bbac7b6ae
Gian Marco <49276912+Metallium96@users.noreply.github.com>
2021-05-08 11:22:20 +0200

    Update eserciziario-pt3-es33-34-35-36.java

--- eserciziario-pt3-es33-34-35-36.java
+++ eserciziario-pt3-es33-34-35-36.java
@@ -1,35 +1,44 @@
 //Esercizio 3.33
-// Scrivere la segnatura e l’opportuno contratto JML per un metodo che data una sequenza di interi, la ripartisca in due sequenze di interi positivi ed interi negativi.
+//Scrivere la segnatura e l’opportuno contratto JML per un metodo che data una sequenza di interi, la ripartisca in due sequenze di interi positivi ed interi negativi.
 
-//@ ensures (\forall int i; i ≥ 0 && i < pos.length; pos[i] ≥ 0) &&
-(\forall int j; j ≥ 0 && j < neg.length; neg[j] < 0)
+//Seq non è vuoto
+//@ requires (seq != null) && (seq.length > 0)
+//Tutti i numeri in pos sono positivi e sono presenti in seq
+//@ ensures (\forall int i; i ≥ 0 && i < pos.length; pos[i] ≥ 0 && (\exists int j; j >= 0 && j < seq.length; pos[i] == seq[j]))
+//Tutti i numeri in neg sono negativi e sono presenti in seq
+//@ ensures (\forall int i; i ≥ 0 && i < neg.length; neg[i] < 0 && (\exists int j; j >= 0 && j < seq.length; neg[i] == seq[j]))
+//Tutti i numeri in seq sono contenuti in pos o neg
+//@ ensures (\forall int i; i ≥ 0 && i < seq.length; 
+//	(\exists int j; j >= 0 && j < pos.length; seq[i] == pos[j]) || (\exists int j; j >= 0 && j < pos.length; seq[i] == neg[j]))
 void Ripart(int[] seq, int[] pos, int[]neg);
 
 //Esercizio 3.34
 // -  Definire una classe Java avente come attributo un array di interi A e come metodo una funzione booleana pospredomina che restituisce true 
 //    se la somma degli interi positivi in A supera il valore assoluto della somma degli interi negativi di A.
 // -  Definire un contratto JML per il metodo pospredomina.
-
 class Example {
-  private int[] A;
+	private int[] A;
 
-  //@ ensures \result <==> 
-  //@	  ((\sum int i; i ≥ 0 && i < A.length; A[i] ≥ 0 ⇒ A[i]) > 
-  //@	  Math.Abs(\sum int i; i ≥ 0 && i < A.length; A[i] < 0 ⇒ A[i]))
-  public boolean pospredomina () {
-    int sumPos = 0;
-    int sumNeg = 0;
+	//A non è vuoto
+	//@ requires (A != null) && (A.length > 0)
+	//Il risultato tornato deve essere true se la somma dei positivi è maggiore del valore assoluto della somma dei numeri negativi
+	//@ ensures \result <==> 
+	//@	  ((\sum int i; i ≥ 0 && i < A.length; A[i] ≥ 0 ⇒ A[i]) > 
+	//@	  Math.Abs(\sum int i; i ≥ 0 && i < A.length; A[i] < 0 ⇒ A[i]))
+	private boolean pospredomina () {
+		int sumPos = 0;
+		int sumNeg = 0;
 
-    for(int i = 0; i< A.length; i++) {
-      if (A[i] ≥ 0)
-        sumPos += A[i];
+		for(int i = 0; i< A.length; i++) {
+			if (A[i] ≥ 0)
+				sumPos += A[i];
 
-      else
-        sumNeg += A[i];
-    }
+			else
+				sumNeg += A[i];
+		}
 
-    return sumPos > Math.Abs(sumNeg);
-  }
+		return sumPos > Math.Abs(sumNeg);
+	}
 }
 
 //Esercizio 3.35
@@ -39,16 +48,19 @@ class Example {
 class Example {
 	private int[] A;
 
+	//A non è vuoto
+	//@ requires (A != null) && (A.length > 0)
+	//Il risultato è true se la somma degli interi è positiva, falso altrimenti
 	//@ ensures \result <==> (\sum int i; i ≥ 0 && i < A.length; A[i]) > 0)
-	public boolean sommaPositiva() {
-      int sum = 0;
+	private boolean sommaPositiva() {
+		int sum = 0;
 
-      for(int i = 0; i< A.length; i++) {
-        sum += A[i];
-      }
+		for(int i = 0; i< A.length; i++) {
+			sum += A[i];
+		}
 
-      return sumPos > 0;
-  }
+		return sumPos > 0;
+	}
 }
 
 //Esercizio 3.36
@@ -56,28 +68,38 @@ class Example {
 //    ordinati in modo crescente, B di interi positivi e C di interi negativi.
 // -  Dare la segnatura e l’opportuno contratto JML per il metodo ripartisci.
 class Example {
-	int[] A;
-	int[] B;
-	int[] C;
+	private int[] A;
+	private int[] B;
+	private int[] C;
 	
+	//A non è vuoto
+	//@ requires (A != null) && (A.length > 0)
+	//Tutti i numeri di B sono interi positivi, e sono in ordine
 	//@ ensures (\forall int i; i ≥ 0 && i < B.length-1; B[i] ≥ 0 && (B[i] ≤ B[i+1]))
-  //@	  && 
-  //@	  (\forall int i ≥ 0 && i < C.length-1; C[i] < 0 && (C[i] ≤ C[i+1])) && C[C.length-1] < 0
-  void ripartisci() {
-    int idxB = 0;
-    int idxC = 0;
-    for(int i = 0; i< A.length; i++) {
-      if (A[i] ≥ 0) {
-        B[idxB] = A[i];
-        idxB++;
-      }
-      else {
-        C[idxC] = A[i];
-        idxC++;
-      }
-    }
+	//Tutti i numeri di C sono interi negativi e sono i nordine
+  	//@ ensures (\forall int i ≥ 0 && i < C.length-1; C[i] < 0 && (C[i] ≤ C[i+1])) && C[C.length-1] < 0
+	//Tutti i numeri di B sono in A
+	//@ ensures (\forall int i; i ≥ 0 && i < B.length; (\exists int j; j >= 0 && j < A.length; B[i] == A[j]))
+	//Tutti i numeri di C sono in A
+	//@ ensures (\forall int i; i ≥ 0 && i < C.length; (\exists int j; j >= 0 && j < A.length; C[i] == A[j]))
+	//Tutti i numeri di A sono in B o in C
+	//@ ensures (\forall int i; i ≥ 0 && i < A.length;
+	//	(\exists int j; j >= 0 && j < B.length; A[i] == B[j]) || (\exists int j; j >= 0 && j < C.length; A[i] == C[j]))
+	private void ripartisci() {
+		int idxB = 0;
+		int idxC = 0;
+		for(int i = 0; i< A.length; i++) {
+			if (A[i] ≥ 0) {
+				B[idxB] = A[i];
+				idxB++;
+			}
+			else {
+				C[idxC] = A[i];
+				idxC++;
+			}
+		}
 
-    Array.sort(B);
-    Array.sort(C);
-  }
+		Array.sort(B);
+		Array.sort(C);
+	}
 }
